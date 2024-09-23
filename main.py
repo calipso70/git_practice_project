adc84bb1702ee7293ce7576a06d2aa98bbfc0e1d
calipso70 <calipse.antonyan@gmail.com>
2024-09-23 17:10:38 +0400

    name change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def greet(name):
     return f"Welcome, {name}!"
 if __name__ == "__main__":
-    name = input("Enter your name: ")
+    name = input("what is your name git : ")
     print(greet(name))
